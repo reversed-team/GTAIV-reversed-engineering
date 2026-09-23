950aba84f73fc3c755d3b655e0e830009a5b3700
imdontknow <miguelviegas2012@outlook.com>
2026-09-23 16:52:07 -0300

    #add script.cpp
    - initial

---
+++ script_commands/script.cpp
@@ -0,0 +1,32 @@
+/* 0x8AE060 */
+
+void RegisterScriptCommands()
+{
+  SetupScriptCommands();
+  SetupScriptCommands();
+  SetupScriptCommands();
+  SetupScriptCommands();
+  SetupScriptCommands();
+  SetupScriptCommands();
+  SetupScriptCommands();
+  SetupScriptCommands();
+  SetupScriptCommands();
+  SetupScriptCommands();
+  SetupScriptCommands();
+  SetupScriptCommands();
+  SetupScriptCommands();
+  SetupScriptCommands();
+  SetupScriptCommands();
+  SetupScriptCommands();
+  SetupScriptCommands();
+  SetupScriptCommands();
+  SetupScriptCommands();
+  SetupScriptCommands();
+  SetupScriptCommands();
+  SetupScriptCommands();
+  SetupScriptCommands();
+  SetupScriptCommands();
+  SetupScriptCommands();
+  SetupScriptCommands();
+  SetupScriptCommands();
+}
